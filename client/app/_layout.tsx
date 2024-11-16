640d040bb9fdb7bc558cb87a822f6c6c725103d2
SAKTHIVAISHNAVI N <sakthivaishnavi12@gmail.com>
2024-11-16 16:33:14 +0530

    changes

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -6,38 +6,46 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { VehicleProvider } from '@/context/VehicleContext';
-SplashScreen.preventAutoHideAsync();
-export default function RootLayout() {
 
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
     }
   }, [loaded]);
-  if (!loaded) {
-    return null;
-  }
-  const theme ={
-     
-  }
-  return (
-    <ThemeProvider value={DarkTheme}>
-      <VehicleProvider>
 
-      <Stack screenOptions={
-        {
-          headerShown: false,
-        }
-        
-      }>
-        <Stack.Screen name="main" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+  if (!loaded) {
+    return null; // Return nothing until fonts are loaded
+  }
+
+  const customTheme = {
+    ...DarkTheme, 
+    colors: {
+      ...DarkTheme.colors, 
+      background: '#121212', 
+      text: '#FFFFFF', 
+    },
+  };
+
+  return (
+    <ThemeProvider value={customTheme}>
+      <VehicleProvider>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="main" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
       </VehicleProvider>
     </ThemeProvider>
   );
--- client/app/main.tsx
+++ client/app/main.tsx
@@ -6,7 +6,7 @@ const main = () => {
   const router = useRouter(); // Initialize router
   useFocusEffect(() => {
     console.log('Focused');
-    router.replace('/(tabs)/analytics')
+    router.replace('/(tabs)/')
   }
   );
   return (
